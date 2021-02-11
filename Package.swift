76235b94d3c0d6b213152ce36f94544f9fd6bfeb
Kevin Renskers <kevin@loopwerk.io>
2021-02-11 12:50:07 +0100

    Adding an AtomFeed helper

--- Package.swift
+++ Package.swift
@@ -4,18 +4,23 @@ import PackageDescription
 
 let package = Package(
   name: "SagaSwimRenderer",
+  platforms: [
+    .macOS(.v10_12)
+  ],
   products: [
     .library(
       name: "SagaSwimRenderer",
       targets: ["SagaSwimRenderer"]),
   ],
   dependencies: [
+    .package(name: "Saga", url: "https://github.com/loopwerk/Saga.git", from: "0.14.0"),
     .package(name: "HTML", url: "https://github.com/robb/Swim", from: "0.1.1"),
   ],
   targets: [
     .target(
       name: "SagaSwimRenderer",
       dependencies: [
+        "Saga",
         "HTML",
       ]),
     .testTarget(
---
+++ Sources/SagaSwimRenderer/AtomFeed.swift
@@ -0,0 +1,131 @@
+import Foundation
+import HTML
+import Saga
+
+public struct AtomFeed {
+  let dateFormatter = ISO8601DateFormatter()
+  let title: String
+  let author: String
+  let baseURL: URL /// the base path of your website, for example https://www.loopwerk.io
+  let pagesPath: String /// the relative path of your list of pages, for example articles/
+  let feedPath: String /// the relative path where this feed will be hosted, for example articles/feed.xml
+  let pages: [AnyPage]
+  let summary: ((AnyPage) -> String?)?
+
+  public init(title: String, author: String, baseURL: URL, pagesPath: String, feedPath: String, pages: [AnyPage], summary: ((AnyPage) -> String?)? = nil) {
+    self.title = title
+    self.author = author
+    self.baseURL = baseURL
+    self.pagesPath = pagesPath
+    self.feedPath = feedPath
+    self.pages = pages
+    self.summary = summary
+  }
+
+  func content() -> Node {
+    feed(xmlns: "http://www.w3.org/2005/Atom") {
+      author {
+        name {
+          self.author
+        }
+      }
+      title {
+        self.title
+      }
+      id {
+        baseURL.appendingPathComponent(pagesPath).absoluteString
+      }
+      link(href: baseURL.appendingPathComponent(feedPath).absoluteString, rel: "self")
+      link(href: baseURL.appendingPathComponent(pagesPath).absoluteString)
+      updated {
+        Date()
+      }
+      pages.map { page in
+        entry {
+          title {
+            page.title.addingXMLEncoding()
+          }
+          id {
+            baseURL.appendingPathComponent(page.url).absoluteString
+          }
+          link(href: baseURL.appendingPathComponent(page.url).absoluteString, rel: "alternate")
+          updated {
+            page.date
+          }
+
+          if let summaryString = self.summary?(page) {
+            summary {
+              summaryString.addingXMLEncoding()
+            }
+          }
+
+          content(type: "html") {
+            Node.text("<![CDATA[")
+            page.body
+            Node.text("]]>")
+          }
+        }
+      }
+    }
+  }
+}
+
+public extension AtomFeed {
+  func feed(xmlns: String, @NodeBuilder children: () -> NodeConvertible) -> Node {
+    .element("feed", [ "xmlns": xmlns ], children().asNode())
+  }
+
+  func author(@NodeBuilder children: () -> NodeConvertible) -> Node {
+    .element("author", [:], children().asNode())
+  }
+
+  func name(children: () -> String) -> Node {
+    .element("name", [:], children().asNode())
+  }
+
+  func title(type: String = "text", children: () -> String) -> Node {
+    .element("title", [ "type": type ], %children().asNode()%)
+  }
+
+  func id(children: () -> String) -> Node {
+    .element("id", [:], %children().asNode()%)
+  }
+
+  func updated(date: () -> Date) -> Node {
+    .element("updated", [:], %.text(dateFormatter.string(from: date()))%)
+  }
+
+  func entry(@NodeBuilder children: () -> NodeConvertible) -> Node {
+    .element("entry", [:], children().asNode())
+  }
+
+  func link(href: String) -> Node {
+    .element("link", [ "href": href ], [])
+  }
+
+  func link(href: String, rel: String) -> Node {
+    .element("link", [ "href": href, "rel": rel ], [])
+  }
+
+  func summary(type: String = "text", children: () -> String) -> Node {
+    .element("summary", [ "type": type ], children().asNode())
+  }
+
+  func content(type: String = "text", @NodeBuilder children: () -> NodeConvertible) -> Node {
+    .element("content", [ "type": type ], children().asNode())
+  }
+}
+
+public extension String {
+  func addingXMLEncoding() -> String {
+    withCFString { string -> NSString in
+      CFXMLCreateStringByEscapingEntities(nil, string, nil)
+    } as String
+  }
+
+  private func withCFString<Result>(_ body: (CFString) throws -> Result) rethrows -> Result {
+    try withCString { cString in
+      try body(CFStringCreateWithCString(nil, cString, CFStringBuiltInEncodings.UTF8.rawValue))
+    }
+  }
+}
