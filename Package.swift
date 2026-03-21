e41e663985fed91f7db8906aea31f42cb72e3cf2
Kevin Renskers <kevin@loopwerk.io>
2026-03-21 20:23:40 +0100

    feat: Declare Saga 3 compatibility

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/loopwerk/Saga.git", from: "2.0.0"),
+    .package(url: "https://github.com/loopwerk/Saga.git", "2.0.3"..<"4.0.0"),
     .package(url: "https://github.com/robb/Swim", from: "0.3.0"),
   ],
   targets: [
